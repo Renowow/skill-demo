dc53cd1c598df54af819c11f9a21e24ff9d5ab94
Renowow <103221818+Renowow@users.noreply.github.com>
2022-05-01 18:23:58 -0700

    Update skillDemoTest.java

--- skillDemoTest.java
+++ skillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class skillDemoTest {
     @Test
     public void skilDemoTest(){
-        assertEquals(6,skillDemo.mutiplication(2, 4));//should fail initially
+        assertEquals(6,skillDemo.mutiplication(2, 3));//should pass
     }
 }
